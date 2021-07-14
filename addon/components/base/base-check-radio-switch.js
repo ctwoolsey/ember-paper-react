eae148deaeff9aeaa8e3ffe916ff29764762c002
ctwoolsey <clark.woolsey@gmail.com>
2021-07-14 10:48:36 +0200

    made separate switch, radio, checkbox functionality

---
+++ addon/components/base/base-check-radio-switch.js
@@ -0,0 +1,228 @@
+import Component from '@glimmer/component';
+/*import { ReactCheckbox, ReactSwitch, ReactRadio} from "../../react-component-lib/";*/
+import { ReactCheckbox } from "../../react-component-lib/react-checkbox";
+import { ReactRadio } from "../../react-component-lib/react-radio";
+import { ReactSwitch } from "../../react-component-lib/react-switch";
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { scheduleOnce } from "@ember/runloop";
+import { action } from "@ember/object";
+import { inject as service } from '@ember/service';
+
+export default class BaseCheckRadioSwitchComponent extends Component {
+  @service themeManager;
+
+  constructor() {
+    super(...arguments);
+    this.reactRef = null;
+    this.controlRef = null;
+
+    this.el = null;
+    this.handleClick = this.handleClick.bind(this);
+
+    this.addedStyles = [];
+    if (this.args.style) {
+      this.addedStyles = this.args.style.split(';');
+    }
+  }
+
+  handleClick() {
+    return (this.args.onClick && this.args.onClick()) || null;
+  }
+
+  @action
+  class() {
+    if (this.reactRef) {
+      this.reactRef.current.setClass(this.args.class || false);
+    }
+  }
+
+  @action
+  style() {
+    if (this.reactRef) {
+      //remove old style names
+      this.addedStyles.forEach(styleItem => {
+        let stylePieces = styleItem.split(':');
+        this.reactRef.current.componentRef.current.style.removeProperty(stylePieces[0]);
+      });
+      //save new style list
+      this.addedStyles = this.args.style.split(';');
+      this.addedStyles.forEach(styleItem => {
+        let stylePieces = styleItem.split(':');
+        this.reactRef.current.componentRef.current.style[stylePieces[0]] = stylePieces[1];
+      });
+
+    }
+  }
+
+  @action
+  checked() {
+    if (this.reactRef) {
+      this.reactRef.current.setChecked(this.args.checked || null);
+    }
+  }
+
+  @action
+  color() {
+    if (this.reactRef) {
+      this.reactRef.current.setColor(this.args.color || null);
+    }
+  }
+
+  @action
+  disabled() {
+    if (this.reactRef) {
+      this.reactRef.current.setDisabled(this.args.disabled || null);
+    }
+  }
+
+  @action
+  disableRipple() {
+    if (this.reactRef) {
+      this.reactRef.current.setDisableRipple(this.args.disableRipple || null);
+    }
+  }
+
+  @action
+  edge() {
+    if (this.reactRef) {
+      this.reactRef.current.setEdge(this.args.edge || false);
+    }
+  }
+
+  @action
+  indeterminate() {
+    if (this.reactRef) {
+      this.reactRef.current.setIndeterminate(this.args.indeterminate || null);
+    }
+  }
+
+  @action
+  label() {
+    if (this.reactRef) {
+      this.reactRef.current.setLabel(this.args.label || null);
+    }
+  }
+
+  @action
+  labelPlacement() {
+    if (this.reactRef) {
+      this.reactRef.current.setLabelPlacement(this.args.labelPlacement || null);
+    }
+  }
+
+  @action
+  required() {
+    if (this.reactRef) {
+      this.reactRef.current.setRequired(this.args.required || null);
+    }
+  }
+
+  @action
+  size() {
+    if (this.reactRef) {
+      this.reactRef.current.setSize(this.args.size || null);
+    }
+  }
+
+  @action
+  value() {
+    if (this.reactRef) {
+      this.reactRef.current.setValue(this.args.value || '');
+    }
+  }
+
+  @action
+  globalThemeChanged() {
+    if (this.reactRef) {
+      this.reactRef.current.setTheme(this.themeManager.theme);
+    }
+  }
+
+  renderElement() {
+    this.el.insertAdjacentElement('afterend', this.reactRef.current.componentRef.current);
+
+    this.cloneAttributes(this.reactRef.current.componentRef.current, this.el);
+    this.initializeDynamicStyles();
+    this.el.remove();
+  }
+
+  cloneAttributes(target, source) {
+    [...source.attributes].forEach( attr => {
+      if (attr.nodeName === 'style') {
+        let styleArr = attr.nodeValue.split(';');
+        styleArr.forEach(style => {
+          let stylePieces = style.split(':');
+          target.style[stylePieces[0]] = stylePieces[1];
+
+        });
+      } else if (attr.nodeName !== 'class') { //ignore class
+        target.setAttribute(attr.nodeName, attr.nodeValue)
+      }
+    });
+  }
+
+  initializeDynamicStyles() {
+    this.addedStyles.forEach(styleItem => {
+      let stylePieces = styleItem.split(':');
+      this.reactRef.current.componentRef.current.style[stylePieces[0]] = stylePieces[1];
+    });
+  }
+
+  @action
+  inserted(element) {
+    this.el = element;
+    scheduleOnce('render', this, this.renderElement);
+
+    this.reactRef = React.createRef();
+    this.controlRef = React.createRef();
+
+    /* What is childRef vs controlRef, etc... */
+
+    let props = {
+      checked: this.args.checked || false,
+      classString: this.args.class || '',
+      color: this.args.color || null,
+      disabled: this.args.disabled || false,
+      disableRipple: this.args.disableRipple || null,
+      edge: this.args.edge || false,
+      indeterminate: this.args.indeterminate || false,
+      label: this.args.label || null,
+      labelPlacement: this.args.labelPlacement || 'end',
+      size: this.args.size || null,
+      theme: this.themeManager.theme || null,
+      value: this.args.value || '',
+      controlRef: this.controlRef,
+      //childRef: this.reactRef,
+      ref: this.reactRef,
+      onChange: this.handleClick
+    };
+
+    let ControlComponent;
+    switch(this.controlType) {
+      case 'checkbox':
+        ControlComponent = ReactCheckbox;
+        delete props.edge;
+        break;
+      case 'radio':
+        ControlComponent = ReactRadio;
+        delete props.edge;
+        delete props.indeterminate;
+        break;
+      case 'switch':
+        ControlComponent = ReactSwitch;
+        delete props.indeterminate;
+        break;
+    }
+    const reactPortal = ReactDOM.createPortal(<ControlComponent {...props}/>, element.parentElement);
+
+    ReactDOM.render(reactPortal, document.createElement('div'));
+
+  }
+
+  willDestroy() {
+    ReactDOM.unmountComponentAtNode(this.reactRef);
+    super.willDestroy();
+  }
+
+}
--- addon/components/r-paper-radio.js
+++ addon/components/r-paper-radio.js
@@ -1,8 +1,8 @@
 import BaseCheckRadioSwitchComponent from "./base/base-check-radio-switch";
 
-export default class RPaperCheckbox extends BaseCheckRadioSwitchComponent {
+export default class RPaperRadioComponent extends BaseCheckRadioSwitchComponent {
   constructor() {
     super(...arguments);
-    this.controlType = 'checkbox';
+    this.controlType = 'radio';
   }
 }
