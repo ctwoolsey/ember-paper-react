5a4a7d708815f68aba81772b476044bc0310872f
ctwoolsey <clark.woolsey@gmail.com>
2021-07-20 15:37:28 +0200

    initial tooltip creation

---
+++ addon/components/r-paper-tooltip.js
@@ -0,0 +1,125 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { scheduleOnce } from "@ember/runloop";
+import { action } from "@ember/object";
+import { COMPONENT_TYPES } from "../react-component-lib/constants/constants";
+import BaseReactEmberComponent from "./base/base-react-ember";
+import { ReactTooltip } from "../react-component-lib/react-tooltip";
+
+export default class RPaperTooltipComponent extends BaseReactEmberComponent {
+
+  constructor() {
+    super(...arguments);
+    this.controlType = COMPONENT_TYPES.AUTOCOMPLETE;
+    this.handleClickChange = null;
+
+  }
+
+  @action
+  title() {
+    if (this.reactRef) {
+      this.reactRef.current.setTitle(this.args.title || null);
+    }
+  }
+
+  @action
+  disableFocusListener() {
+    if (this.reactRef) {
+      this.reactRef.current.setDisableFocusListener(this.args.disableFocusListener || null);
+    }
+  }
+
+  @action
+  disableHoverListener() {
+    if (this.reactRef) {
+      this.reactRef.current.setDisableHoverListener(this.args.disableHoverListener || null);
+    }
+  }
+
+  @action
+  disableInteractive() {
+    if (this.reactRef) {
+      this.reactRef.current.setDisableInteractive(this.args.disableInteractive || null);
+    }
+  }
+
+  @action
+  disableTouchListener() {
+    if (this.reactRef) {
+      this.reactRef.current.setDisableTouchListener(this.args.disableTouchListener || null);
+    }
+  }
+
+  @action
+  open() {
+    if (this.reactRef) {
+      this.reactRef.current.setOpen(this.args.open || null);
+    }
+  }
+
+  @action
+  placement() {
+    if (this.reactRef) {
+      this.reactRef.current.setPlacement(this.args.placement || null);
+    }
+  }
+
+  @action
+  sx() {
+    if (this.reactRef) {
+      this.reactRef.current.setSx(this.args.sx || null);
+    }
+  }
+
+  renderElement() {
+   /* if (this.args.select && this.el.hasChildNodes()) {
+      this.reactRef.current.componentRef.current.getElementsByClassName('MuiSelect-select')[0].replaceChildren(this.fragmentFromBlockContent());
+    }*/
+    super.renderElement();
+  }
+
+  @action
+  inserted(element) {
+    this.el = element;
+    scheduleOnce('render', this, this.renderElement);
+
+    this.reactRef = React.createRef();
+
+    let props = {
+      title: this.args.title || null,
+      arrow: this.args.arrow || null,
+      classString: this.args.class || '',
+      describeChild: this.args.describeChild || null,
+      disableFocusListener: this.args.disableFocusListener || null,
+      disableHoverListener: this.args.disableHoverListener || null,
+      disableInteractive: this.args.disableInteractive || null,
+      disableTouchListener: this.args.disableTouchListener || null,
+      enterDelay: this.args.enterDelay || null,
+      enterNextDelay: this.args.enterNextDelay || null,
+      enterTouchDelay: this.args.enterTouchDelay || null,
+      followCursor: this.args.followCursor || null,
+      id: this.args.id || null,
+      leaveDelay: this.args.leaveDelay || null,
+      leaveTouchDelay: this.args.leaveTouchDelay || null,
+      onClose: this.args.onClose || null,
+      onOpen: this.args.onOpen || null,
+      open: this.args.open ||  null,
+      placement: this.args.placement || null,
+      popperComponent: this.args.popperComponent || null,
+      popperProps: this.args.popperProps || null,
+      sx: this.args.sx || null,
+      theme: this.themeManager.theme || null,
+      transitionComponent: this.args.transitionComponent || null,
+      transitionProps: this.args.TransitionProps || null,
+      ref: this.reactRef
+    };
+
+    const reactPortal = ReactDOM.createPortal(<ReactTooltip {...props}/>, element.parentElement);
+
+    ReactDOM.render(reactPortal, document.createElement('div'));
+
+  }
+}
+
+
+
---
+++ addon/react-component-lib/react-tooltip.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import TextField from '@material-ui/core/TextField';
+import { ReactThemeBase } from "./base/react-theme-base";
+import { ReactConditionalThemeProvider } from "./react-conditional-theme-provider";
+
+export class ReactAutocomplete extends ReactThemeBase{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: props.title,
+      classString: props.classString,
+      disableFocusListener: props.disableFocusListener,
+      disableHoverListener: props.disableHoverListener,
+      disableInteractive: props.disableInteractive,
+      disableTouchListener: props.disableTouchListener,
+      open: props.open,
+      placement: props.placement,
+      sx: props.sx,
+      theme: props.theme,
+    };
+
+    this.componentRef = React.createRef();
+
+    //methods
+    this.setTitle = this.setTitle.bind(this);
+    this.setClass = this.setClass.bind(this);
+    this.setDisableFocusListener = this.setDisableFocusListener.bind(this);
+    this.setDisableHoverListener = this.setDisableHoverListener.bind(this);
+    this.setDisableInteractive = this.setDisableInteractive.bind(this);
+    this.setDisableTouchListener = this.setDisableTouchListener.bind(this);
+    this.setOpen = this.setOpen.bind(this);
+    this.setPlacement = this.setPlacement.bind(this);
+    this.setSx = this.setSx.bind(this);
+    this.setTheme = this.setTheme.bind(this);
+  }
+
+  setTitle(title) {
+    this.setState({title: title})
+  }
+
+  setClass(classes) {
+    this.setState({classString: classes})
+  }
+
+  setDisableFocusListener(disableFocusListener) {
+    this.setState( {disableFocusListener: disableFocusListener});
+  }
+
+  setDisableHoverListener(disableHoverListener) {
+    this.setState( {disableHoverListener: disableHoverListener});
+  }
+
+  setDisableInteractive(disableInteractive) {
+    this.setState( {disableInteractive: disableInteractive});
+  }
+
+  setDisableTouchListener(disableTouchListener) {
+    this.setState({ disableTouchListener: disableTouchListener });
+  }
+
+  setOpen(open) {
+    this.setState( {open: open});
+  }
+
+  setPlacement(placement) {
+    this.setState( {placement: placement});
+  }
+
+  setSx(sx) {
+    this.setState({ sx: sx});
+  }
+
+  setTheme(theme) {
+    this.setState( {theme: theme});
+  }
+
+  render() {
+    const {
+      title,
+      classString,
+      disableFocusListener,
+      disableHoverListener,
+      disableInteractive,
+      disableTouchListener,
+      open,
+      placement,
+      sx,
+      theme
+    } = this.state;
+
+    return (
+      <ReactConditionalThemeProvider theme={theme}>
+        <Tooltip
+          ref={this.componentRef}
+          {...(title ? {title: title} : {title: ''})}
+          {...(this.props.arrow ? {arrow: this.props.arrow} : {})}
+          {...(classString ? {className: classString} : {})}
+          {...(this.props.describeChild ? {describeChild: this.props.describeChild} : {})}
+          {...(disableFocusListener ? {disableFocusListener: disableFocusListener} : {})}
+          {...(disableHoverListener ? {disableHoverListener: disableHoverListener} : {})}
+          {...(disableInteractive ? {disableInteractive: disableInteractive} : {})}
+          {...(disableTouchListener ? {disableTouchListener: disableTouchListener} : {})}
+          {...(this.props.enterDelay ? {enterDelay: this.props.enterDelay} : {})}
+          {...(this.props.enterNextDelay ? {enterNextDelay: this.props.enterNextDelay} : {})}
+          {...(this.props.enterTouchDelay ? {enterTouchDelay: this.props.enterTouchDelay} : {})}
+          {...(this.props.followCursor ? {followCursor: this.props.followCursor} : {})}
+          {...(this.props.id ? {id: this.props.id} : {})}
+          {...(this.props.leaveDelay ? {leaveDelay: this.props.leaveDelay} : {})}
+          {...(this.props.leaveTouchDelay ? {leaveTouchDelay: this.props.leaveTouchDelay} : {})}
+          {...(this.props.onClose ? {onClose: this.props.onClose} : {})}
+          {...(this.props.onOpen ? {onOpen: this.props.onOpen} : {})}
+          {...(open ? {open: open} : {})}
+          {...(placement ? {placement: placement} : {})}
+          {...(this.props.popperComponent ? {PopperComponent: this.props.popperComponent} : {})}
+          {...(this.props.popperProps ? {PopperProps: this.props.popperProps} : {})}
+          {...(sx ? {sx: sx} : {})}
+          {...(this.props.transitionComponent ? {TransitionComponent: this.props.transitionComponent} : {})}
+          {...(this.props.transitionProps ? {TransitionProps: this.props.transitionProps} : {})}
+        >
+          <span>Hover over me</span>
+        </Tooltip>
+      </ReactConditionalThemeProvider>
+    );
+  }
+}
