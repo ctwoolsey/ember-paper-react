574545bb5dd1a7da0923827709a9ca8602dff67f
ctwoolsey <clark.woolsey@gmail.com>
2021-10-01 13:26:32 +0200

    made icons load only on props and state props

--- addon/react-component-lib/utility/react-prop-sifter.js
+++ addon/react-component-lib/utility/react-prop-sifter.js
@@ -17,15 +17,16 @@ function reactPropSifter(componentProps, propsObj) {
     let value = componentProps[propName];
     if (value && propName.toLowerCase().endsWith('icon')) {
       value = reactCreateIcon(value);
+      //icons will not update any props or state props not for the component
     }
     if (Object.prototype.hasOwnProperty.call(stateProps, propName)) {
       siftedProps.stateProps[propName] = value;
     } else {
       if (Object.prototype.hasOwnProperty.call(statefulPropsNotForComponent, propName)) {
-        siftedProps.statefulPropsNotForComponent[propName] = value;
+        siftedProps.statefulPropsNotForComponent[propName] = componentProps[propName];
       } else {
         if (Object.prototype.hasOwnProperty.call(propsNotForComponent, propName)) {
-          siftedProps.propsNotForComponent[propName] = value;
+          siftedProps.propsNotForComponent[propName] = componentProps[propName];
         } else {
           if (Object.prototype.hasOwnProperty.call(props, propName)) {
             siftedProps.staticProps[propName] = value;
