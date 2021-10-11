25e12bbba772adf9377a6d6104e9880be69c4c5b
ctwoolsey <clark.woolsey@gmail.com>
2021-10-11 14:49:40 +0200

    base code test not working

--- tests/integration/components/base-code-test.js
+++ tests/integration/components/base-code-test.js
@@ -1,8 +1,9 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { BaseEmberPaperReact } from 'ember-paper-react/components/base/base-ember-paper-react';
+import BaseEmberPaperReact from 'ember-paper-react/components/base/base-ember-paper-react';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { TestPropObj } from "../../../addon/prop-files/test/test-props";
 
 module('Integration | Component | base-code', function(hooks) {
   setupRenderingTest(hooks);
@@ -10,9 +11,13 @@ module('Integration | Component | base-code', function(hooks) {
   test('it renders', async function(assert) {
     class MyTestComponent extends BaseEmberPaperReact {
       // overrides
+      /*constructor() {
+        super(...arguments);
+        console.log('I WAS CREATED***********');
+      }*/
     }
     this.setProperties({ MyTestComponent });
-  //  await render(hbs`<MyTestComponent />`);
+    //await render(hbs`<MyTestComponent />`);
     assert.equal(1, 1);
   });
 });
