8a1a1b987beda9c71835422c2ccb21b1880a011d
ctwoolsey <clark.woolsey@gmail.com>
2021-10-13 08:55:44 +0200

    r-paper-test dynamic content

--- tests/integration/components/r-paper-test.js
+++ tests/integration/components/r-paper-test.js
@@ -1,13 +1,14 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, settled } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { next } from '@ember/runloop';
+import { tracked } from '@glimmer/tracking';
 
 module('Integration | Component | r-paper', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders content into the correct location', async function(assert) {
     await render(hbs`
       <RPaper @id="test-me">
         template block text
@@ -18,6 +19,38 @@ module('Integration | Component | r-paper', function(hooks) {
       const testElement = document.getElementById('test-me');
       assert.equal(testElement.textContent.trim(), 'template block text');
     });
+  });
 
+  test('it renders dynamic content correctly', async function(assert) {
+    class MyContext {
+      @tracked eachTest = [1,2,3];
+    }
+    let ctx = new MyContext();
+    this.setProperties({ctx});
+
+    await render(hbs`
+      <RPaper @id="test-me">
+        {{#each this.ctx.eachTest}}
+          <div>A</div>
+        {{/each}}
+      </RPaper>
+    `);
+
+    let testElement;
+
+    next(this, function() {
+      testElement = document.getElementById('test-me');
+      assert.equal(testElement.childElementCount, 3, `Children count does not match initial count`);
+    });
+
+    await settled();
+    ctx.eachTest = [1,2,3,4];
+    await settled();
+    assert.equal(testElement.childElementCount, 4, `Children count does not match added count`);
+
+    await settled();
+    ctx.eachTest = [1,2];
+    await settled();
+    assert.equal(testElement.childElementCount, 2, `Children count does not match removed count`);
   });
 });
