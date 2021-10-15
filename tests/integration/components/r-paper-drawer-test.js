5d758126eed2701c38d4ae84d7f9e576408d15aa
ctwoolsey <clark.woolsey@gmail.com>
2021-10-15 15:42:44 +0200

    r-paper-drawer tests

--- tests/integration/components/r-paper-drawer-test.js
+++ tests/integration/components/r-paper-drawer-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, settled } from '@ember/test-helpers';
+import { A } from '@ember/array';
 import { hbs } from 'ember-cli-htmlbars';
 import { tracked } from "@glimmer/tracking";
 
@@ -41,6 +42,7 @@ module('Integration | Component | r-paper-drawer', function(hooks) {
   test('it renders and opens dynamic drawer', async function(assert) {
     class MyContext {
       @tracked drawerOpen = false;
+      @tracked contentList = A(['A', 'B', 'C']);
     }
     let ctx = new MyContext();
     this.setProperties({ctx});
@@ -48,7 +50,9 @@ module('Integration | Component | r-paper-drawer', function(hooks) {
     await render(hbs`
       <div>
         <RPaperDrawer @id='test-me' @open={{this.ctx.drawerOpen}} >
-          <div>drawer content</div>
+          {{#each this.ctx.contentList as |contentItem|}}
+            <div>{{contentItem}}</div>
+          {{/each}}
         </RPaperDrawer>
       </div>
     `);
@@ -63,7 +67,24 @@ module('Integration | Component | r-paper-drawer', function(hooks) {
     drawerElement = document.getElementById('test-me');
     assert.ok(drawerElement, `drawer opened`);
     await settled();
-    assert.equal(drawerElement.textContent.trim(), 'drawer content', `drawer content rendered correctly`);
+    let contentContainer = drawerElement.querySelector('.MuiDrawer-paper');
+    assert.equal(contentContainer.children.length, 3, `rendered children correctly`);
+    assert.equal(contentContainer.children[0].textContent.trim(), 'A', `'A' rendered as expected`);
+    assert.equal(contentContainer.children[1].textContent.trim(), 'B', `'B' rendered as expected`);
+    assert.equal(contentContainer.children[2].textContent.trim(), 'C', `'C' rendered as expected`);
+
+    this.ctx.contentList = A(['A', 'B', 'C', 'D']);
+    await settled();
+    contentContainer = drawerElement.querySelector('.MuiDrawer-paper');
+    assert.equal(contentContainer.children.length, 4, `rendered added children correctly`);
+    assert.equal(contentContainer.children[3].textContent.trim(), 'D', `'D' rendered as expected`);
+
+    this.ctx.contentList = A(['A', 'B']);
+    await settled();
+    contentContainer = drawerElement.querySelector('.MuiDrawer-paper');
+    assert.equal(contentContainer.children.length, 2, `rendered removed children correctly`);
+    assert.equal(contentContainer.children[0].textContent.trim(), 'A', `'A' rendered as expected`);
+    assert.equal(contentContainer.children[1].textContent.trim(), 'B', `'B' rendered as expected`);
 
     this.ctx.drawerOpen = false;
 
