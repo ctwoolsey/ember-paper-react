ad60c50e95c9e1f9a17142ca36b0b8a8d9e5bd2d
ctwoolsey <clark.woolsey@gmail.com>
2022-01-14 18:36:09 +0100

    tests created

--- tests/integration/components/r-paper-i-mask-text-field-test.js
+++ tests/integration/components/r-paper-i-mask-text-field-test.js
@@ -1,10 +1,69 @@
-import { module} from 'qunit';
+import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { RunStandardTests } from "../standard-tests/rendering-tests";
+import { render, fillIn } from '@ember/test-helpers';
+import { next } from '@ember/runloop';
+import { TestSimpleRender } from "../standard-tests/rendering-tests";
+import { hbs } from 'ember-cli-htmlbars';
+import { tracked } from "@glimmer/tracking";
 
 module('Integration | Component | r-paper-i-mask-text-field', function(hooks) {
   setupRenderingTest(hooks);
 
-  RunStandardTests('RPaperIMaskTextField');
+  TestSimpleRender('RPaperIMaskTextField');
+
+  test('it displays the correct text value', async function(assert) {
+
+    await render(hbs`
+      <RPaperTextField @id="test-me" @value="some-value" />
+    `);
+
+    next(this, function() {
+      const testElement = document.getElementById('test-me');
+      assert.equal(testElement.nodeName, 'INPUT', `element type is an 'input'`);
+      assert.equal(testElement.value, 'some-value', `input value matched passed value`);
+    });
+  });
+
+  test('it applies the input mask correctly', async function(assert) {
+    class MyContext {
+      @tracked inputMaskTextValue = null;
+      @tracked inputUnMaskedTextValue = null;
+    }
+    let ctx = new MyContext();
+    this.setProperties({ctx});
+
+    this.set('blocks', {
+      num: {
+        mask: Number,
+        thousandsSeparator: ','
+      }
+    });
+
+    this.set('mask', '$num');
+
+    this.set('onInputTextInputMaskChanged', (evt) => {
+      this.ctx.inputMaskTextValue = evt.target.maskedValue;
+      this.ctx.inputUnMaskedTextValue =evt.target.value;
+    });
+
+
+    await render(hbs`
+      <div>
+        <RPaperIMaskTextField @id="test-me" @mask={{this.mask}} @blocks={{this.blocks}} @nativeOnChange={{true}} @value="1234" @onChange={{this.onInputTextInputMaskChanged}} />
+      </div>
+    `);
+
+    next(this, async function() {
+      const testElement = document.getElementById('test-me');
+      assert.equal(testElement.nodeName, 'INPUT', `element type is not an 'input'`);
+      assert.equal(testElement.value, '$1,234', `masked text value is what it should be: '$1,234'`)
+      await fillIn(testElement, '1235');
+      assert.equal(this.ctx.inputUnMaskedTextValue, '1235', `unmasked text value is what it should be: '1235'`);
+      assert.equal(this.ctx.inputMaskTextValue, '$1,235', `masked text value is what it should be: '$1,235`);
+    });
+  });
 });
+
+
+
 
--- tests/unit/services/input-mask-types-test.js
+++ tests/unit/services/input-mask-types-test.js
@@ -8,4 +8,17 @@ module('Unit | Service | input-mask-types', function(hooks) {
     let service = this.owner.lookup('service:input-mask-types');
     assert.ok(service);
   });
+
+  test('sets and retrieves mask types', function(assert) {
+    const obj1 = {a:1};
+    const obj2 = {b:1};
+
+    let service = this.owner.lookup('service:input-mask-types');
+    assert.equal(Object.keys(service.iMaskTypes).length, 0, `iMaskTypes is empty`);
+    assert.equal(Object.keys(service.numberFormatTypes).length, 0, `numberFormatTypes is empty`);
+    service.addIMaskType('a', obj1);
+    service.addNumberFormatType('b', obj2);
+    assert.equal(service.getIMaskType('a'), obj1, `iMaskTypes a correctly returned object`);
+    assert.equal(service.getNumberFormatType('b'), obj2, `numberFormatTypes a correctly returned object`);
+  });
 });
